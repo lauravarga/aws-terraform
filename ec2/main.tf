8f3e6661ec2f5d08d81d3b90e9d86ee2ff080e63
Laura Varga <lolovarga@gmail.com>
2023-02-08 21:02:59 +0100

    fixing the ec2 part

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,9 +6,17 @@ resource "aws_instance" "grafana-ec2" {
   ami             = "ami-06c39ed6b42908a36"
   instance_type   = "t2.micro"
   key_name        = "aws-terraform"
-  security_groups = ["${aws_security_group.grafana-security-group.id}"]
-  subnet_id       = aws_subnet.grafana-subnet-public.id
+  security_groups = ["sg-009f63dbcb79bf7fd"]
+  subnet_id       = "subnet-0763649b34226ba35"
   tags = {
     Name = "grafana"
+  }
+}
+
+resource "aws_eip" "grafana-public-ip" {
+  instance   = aws_instance.grafana-ec2.id
+  vpc        = true
+  tags = {
+    Name = "grafana-public-ip"
   }
 }
